36c0b44384bd081b13ddc0a1a23dc005ed0be419
bcso <bcso@uwaterloo.ca>
2014-11-01 12:41:09 -0400

    added moving axis

--- NamePending/Assets/MovementScript.cs
+++ NamePending/Assets/MovementScript.cs
@@ -9,10 +9,10 @@ public class MovementScript : MonoBehaviour {
 
 	void FixedUpdate()
 	{
-		float horizontal = Input.GetAxis ("Horizontal");
+//		float horizontal = Input.GetAxis ("Horizontal");
 		float vertical = Input.GetAxis ("Vertical");
 		mainCamera = GameObject.FindGameObjectWithTag ("MainCamera");
-
+		
 		Vector3 forceVect = new Vector3 (horizontal, 0.0f, vertical);
 
 		rigidbody.AddForce (forceVect * speed);
