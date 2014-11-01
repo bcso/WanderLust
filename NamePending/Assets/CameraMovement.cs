f209060454861b215d17969a0b548af31fdc8905
bcso <bcso@uwaterloo.ca>
2014-11-01 02:08:48 -0400

    added tags files

--- NamePending/Assets/CameraMovement.cs
+++ NamePending/Assets/CameraMovement.cs
@@ -13,35 +13,11 @@ public class CameraMovement : MonoBehaviour {
 
 	void Awake ()
 	{
-
 		player = GameObject.FindGameObjectWithTag (Tags.player).transform;
 		relCameraPosition = transform.position - player.position;
 		relCameraPosMag = relCameraPosition.magnitude - 0.5f;
 	}
-
-	void FixedUpdate()
-	{
-//		Vector3 standardPosition = player.position + relCameraPosition;
-//		Vector3 abovePosition = player.position + Vector3.up * relCameraMag;
-//
-//		Vector3[] checkPoints = Vector3 [5];
-//
-//		checkPoints [0] = standardPosition;
-//		checkPoints [1] = Vector3.Lerp(standardPosition, abovePosition, 0.25f);
-//		checkPoints [2] = Vector3.Lerp(standardPosition, abovePosition, 0.5f);
-//		checkPoints [3] = Vector3.Lerp(standardPosition, abovePosition, 0.75f);
-//		checkPoints [4] = abovePosition;
-//
-//		for (int i =0; i < checkPoints.Length; i++)
-//		{
-//			if(checkPosition(checkPoints[i]))
-//				break;
-//		}
-
-
-		      
-	}
-
+	
 	void Update()
 	{
 		transform.position = player.transform.position + relCameraPosition;
@@ -49,11 +25,11 @@ public class CameraMovement : MonoBehaviour {
 		checkPosition (relCameraPosition);
 	}
 
-	void checkPosition(Vector3 checkVect)
+	void checkPosition(Vector3 checkPos)
 	{
 		RaycastHit hit;
 
-		if (Physics.Raycast(checkVect, player.position - checkVect, out hit ,relCameraPosMag)){
+		if (Physics.Raycast(checkPos, player.position - checkPos, out hit ,relCameraPosMag)){
 			if (hit.transform != player)
 			{
 				hitGameObject = hit.transform.gameObject;
