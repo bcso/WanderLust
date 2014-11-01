c3da422c1affa5d1f0b91481fe73695e5abbdf77
bcso <bcso@uwaterloo.ca>
2014-10-31 23:12:29 -0400

    added walls + ball movement + basic camera tracking

---
+++ NamePending/Assets/CameraMovement.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMovement : MonoBehaviour {
+
+	public GameObject player;
+	public Vector3 offset;
+
+	void Start ()
+	{
+		offset = transform.position;
+	}
+
+	void Update()
+	{
+		player = GameObject.FindGameObjectWithTag ("Player");
+
+		transform.position = player.transform.position + offset;
+	}
+}
