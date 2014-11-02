ee2eed918b9109cb5e188c8ca5fcba7b9c0d9048
Millin Gabani <gabanimillin@gmail.com>
2014-11-02 01:10:29 -0500

    added the kinect

---
+++ NamePending/Assets/PumpkinScript.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PumpkinScript : MonoBehaviour {
+
+	public float speed;
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (new Vector3 (15, 30, 45) * Time.deltaTime*speed); 
+	}
+}
---
+++ NamePending/Assets/Scripts/Skeleton.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Skeleton : MonoBehaviour {
+
+		public static Vector3 HandLeftPos,
+	HandRightPos,
+	ElbowLeftPos,
+	ElbowRightPos,
+	KneeLeftPos,
+	KneeRightPos,
+	SpineMidPos,
+	ShoulderLeftPos,
+	ShoulderRightPos,
+	FootLeftPos,
+	FootRightPos;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		HandLeftPos = BodySourceView.HandLeftPos;
+		HandRightPos = BodySourceView.HandRightPos;
+		ElbowLeftPos = BodySourceView.ElbowLeftPos;
+		ElbowRightPos = BodySourceView.ElbowRightPos;
+		KneeLeftPos = BodySourceView.KneeLeftPos;
+		KneeRightPos = BodySourceView.KneeRightPos;
+
+		ShoulderLeftPos = BodySourceView.ShoulderLeftPos;
+		ShoulderRightPos = BodySourceView.ShoulderRightPos;
+		FootLeftPos = BodySourceView.FootLeftPos;
+		FootRightPos = BodySourceView.FootRightPos;
+		SpineMidPos = BodySourceView.SpineMidPos;
+		Debug.Log ("SpineMid: " + SpineMidPos);
+		Debug.Log ("ShoulderRight: " + ShoulderRightPos);
+		Debug.Log ("HandRight: " + HandRightPos);
+
+	}
+}
